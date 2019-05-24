1c2eb7db88b0756ca822aa0db9708aeba4aef752
Praveen Visam <Praveen.Visam@sbdinc.com>
2019-05-24 18:14:02 +0530

    Common Libraries added

---
+++ CommonService/Generics/Converter/AsyncLibrary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CommonLiraries
+{
+    public class AsynLibrary
+    {
+        public async Task<T2> GenericAsyncCall<T1, T2>(Func<T1, T2> GenericMethod, T1 input)
+        {
+            var _output = await Task.Factory.StartNew(() => {
+                var output = GenericMethod.Invoke(input);
+                return output;
+            });
+
+            return (T2)_output;
+        }
+    }
+
+
+
+
+
+}
